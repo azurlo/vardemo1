bdd0261b26934e61bb5535cc6804a2a81877c770
aboudnik_at_epam <aboudnik_at_epam@gcp-login1.c.slurm-var-demo.internal>
2018-06-22 00:27:01 +0000

    get rid of temp files

--- run.sh
+++ run.sh
@@ -2,8 +2,6 @@
 basedir=$(dirname $0)
 N=500
 
-rm -rf /tmp/${USER}*
-
-simulate_job=$(sbatch --array=1-${N} ${basedir}/simulate.slurm | cut -f 4 -d' ')
+simulate_job=$(sbatch --array=1-${N} ${basedir}/simulate.slurm ${basedir} | cut -f 4 -d' ')
 
 sbatch --dependency=afterok:${simulate_job} ${basedir}/collect.slurm
